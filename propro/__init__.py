4175ab736f9984abb1d3fe5a2f37a483e3b870c9
Agata Filho <agata78@gmail.com>
2015-01-03 13:41:01 -0500

    Begin work on mock data source.

--- propro/__init__.py
+++ propro/__init__.py
@@ -2,9 +2,30 @@
 
 __version__ = '0.1.0'
 
-def find_foods_by_protein_content(protein_content):
-    """List all foods having at least the specified `protein_content`. The
-    content should be a `float` between 0.0 (meaning "no protein") and 1.0
-    (meaning "all calories come from protein"), inclusive.
+from . import mock
+
+# Factory methods
+
+def mock_source():
+    """() -> Source
+
+    Return a source of dummy data.
+    """
+    return mock.Source()
+
+def usda_source():
+    """() -> Source
+
+    Return a source of data from the USDA.
+    """
+    return usda.Source()    # TODO
+
+# Utility methods
+
+def calories_per_serving(food):
+    """(str) -> float
+
+    Return the number of calories in one serving of `food`; e.g., 30
+    calories in one serving (i.e., 1 oz) of chicken breast.
     """
     pass

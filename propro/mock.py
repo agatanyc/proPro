f345006efb55ce7c3ad7442cf143eecaf278e561
Agata Filho <agata78@gmail.com>
2015-01-10 12:06:49 -0500

    Refactor and add test methods.

--- propro/mock.py
+++ propro/mock.py
@@ -2,9 +2,49 @@
 
 from . import base
 
+# import json
+#
+# _raw = """{
+#     "Chicken": {
+#         "protein_grams_per_oz" :   6,
+#         "oz_per_serving"       :   6,
+#         "calories_per_oz"      :  30 },
+#     "Cottage Cheese": {
+#         "protein_grams_per_oz" :   3,
+#         "oz_per_serving"       :   4,
+#         "calories_per_oz"      :  20 },
+#     "Greek Yogurt": {
+#         "protein_grams_per_oz" :   2.75,
+#         "oz_per_serving"       :   8,
+#         "calories_per_oz"      :  16 },
+#     "Candy": {
+#         "protein_grams_per_oz" :   0,
+#         "oz_per_serving"       :   1,
+#         "calories_per_oz"      :  60 }
+# }"""
+#
+# _data = json.loads(_raw)
+
+PROTEIN_GRAMS_PER_OZ, OZ_PER_SERVING, CALORIES_PER_OZ = range(3)
+
+_data = { 'Chicken':          (     6, 6, 30),
+          'Cottage Cheese':   (     3, 4, 20),
+          'Greek Yogurt':     (  2.75, 8, 16),
+          'Candy':            (     0, 1, 60) }
+
 class Source(base.Source):
     """Protein data source returning only dummy data"""
 
     # Overrides of base Source
 
-    pass # TODO
+    def all_foods(self):
+        return list(_data.keys())
+
+    def calories_per_oz(self, food):
+        return _data[food][CALORIES_PER_OZ]
+
+    def oz_per_serving(self, food):
+        return _data[food][OZ_PER_SERVING]
+
+    def protein_grams_per_oz(self, food):
+        return _data[food][PROTEIN_GRAMS_PER_OZ]
--- tests/test_base.py
+++ tests/test_base.py
@@ -11,20 +11,19 @@ from propro import base
 class TestBaseSourceAbstractMethods(unittest.TestCase):
 
     def setUp(self):
-        self.source = base.Source()
         self.arbitraryContent = 0.5
         self.arbitraryFood = 'chicken'
+        self.source = base.Source()
 
-    def test_foods_by_protein_content(self):
+    def test_all_foods(self):
         self.assertRaises(
                 NotImplementedError,
-                self.source.foods_by_protein_content,
-                self.arbitraryContent)
+                self.source.all_foods)
 
-    def test_protein_grams_per_oz(self):
+    def test_calories_per_oz(self):
         self.assertRaises(
                 NotImplementedError,
-                self.source.protein_grams_per_oz,
+                self.source.calories_per_oz,
                 self.arbitraryFood)
 
     def test_oz_per_serving(self):
@@ -33,29 +32,41 @@ class TestBaseSourceAbstractMethods(unittest.TestCase):
                 self.source.oz_per_serving,
                 self.arbitraryFood)
 
-    def test_calories_per_oz(self):
+    def test_protein_grams_per_oz(self):
         self.assertRaises(
                 NotImplementedError,
-                self.source.calories_per_oz,
+                self.source.protein_grams_per_oz,
                 self.arbitraryFood)
 
 class TestBaseSourceUtilityMethods(unittest.TestCase):
 
-    def setUp(self):
-        self.arbitraryFood = 'chicken'
-
     class DummySource(base.Source):
 
+        def all_foods(self):
+            return ['chicken', 'greek_yogurt']
+
         def calories_per_oz(self, food):
-            return 6
+            return 10
 
         def oz_per_serving(self, food):
-            return 9
+            return 6
+
+        def protein_grams_per_oz(self, food):
+            return 2 if food == 'chicken' else 1
+
+    def setUp(self):
+        self.source = TestBaseSourceUtilityMethods.DummySource()
 
     def test_calories_per_serving(self):
-        source = TestBaseSourceUtilityMethods.DummySource()
-        self.assertEqual(54, source.calories_per_serving(self.arbitraryFood))
+        actual = self.source.calories_per_serving('chicken')
+        self.assertEqual(60, actual)
+
+    def test_foods_by_protein_content(self):
+        actual = self.source.foods_by_protein_content(0.5)
+        self.assertEqual(['chicken'], actual)
+
+    def test_protein_content(self):
+        self.assertEqual(0.8, self.source.protein_content('chicken'))
 
 if __name__ == '__main__':
     unittest.main()
-

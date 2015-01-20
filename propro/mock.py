9ac32b93718b31ac8f50cbabfe42739a6ed11658
Agata Filho <agata78@gmail.com>
2015-01-20 09:48:20 -0500

    Remove dead code.

--- propro/mock.py
+++ propro/mock.py
@@ -2,29 +2,6 @@
 
 from . import base
 
-# import json
-#
-# _raw = """{
-#     "Chicken": {
-#         "protein_grams_per_oz" :   6,
-#         "oz_per_serving"       :   6,
-#         "calories_per_oz"      :  30 },
-#     "Cottage Cheese": {
-#         "protein_grams_per_oz" :   3,
-#         "oz_per_serving"       :   4,
-#         "calories_per_oz"      :  20 },
-#     "Greek Yogurt": {
-#         "protein_grams_per_oz" :   2.75,
-#         "oz_per_serving"       :   8,
-#         "calories_per_oz"      :  16 },
-#     "Candy": {
-#         "protein_grams_per_oz" :   0,
-#         "oz_per_serving"       :   1,
-#         "calories_per_oz"      :  60 }
-# }"""
-#
-# _data = json.loads(_raw)
-
 PROTEIN_GRAMS_PER_OZ, OZ_PER_SERVING, CALORIES_PER_OZ = range(3)
 
 _data = { 'Chicken':          (     6, 6, 30),

26a99d12bfc34d3a488cbe879ccf0dbbeef532ed
Agata Filho <agata78@gmail.com>
2015-01-20 09:49:00 -0500

    Begin testing mock source.

---
+++ tests/test_mock.py
@@ -0,0 +1,40 @@
+"""Test driver for propro.mock"""
+
+import os
+import sys
+import unittest
+
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), os.pardir))
+
+from propro import mock
+
+class TestMockSource(unittest.TestCase):
+
+    def setUp(self):
+        self.source = mock.Source()
+        self.chicken = 'Chicken'
+
+    def test_all_foods_returns_list(self):
+        foods = self.source.all_foods()
+        self.assertEqual(list, type(foods))
+
+    def test_all_foods_returns_non_empty(self):
+        foods = self.source.all_foods()
+        self.assertTrue(foods)
+
+    def test_all_foods_returns_unique_values(self):
+        foods = self.source.all_foods()
+        self.assertEqual(len(foods), len(set(foods)))
+
+    def test_calories_per_oz(self):
+        cal = self.source.calories_per_oz(self.chicken)
+        self.assertGreaterEqual(cal, 0)
+
+    def test_oz_per_serving(self):
+        pass # TODO
+
+    def test_protein_grams_per_oz(self):
+        pass # TODO
+
+if __name__ == '__main__':
+    unittest.main()
